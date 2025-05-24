07011146cba590ce7b7ad8ddacbdf6b00d771c5f
Dh1p <uwazed@gmail.com>
2025-05-24 19:56:26 +0600

    24MAY

--- A_Hit_the_Lottery.c
+++ A_Hit_the_Lottery.c
@@ -8,7 +8,36 @@ int main()
     int n;
     scanf("%d",&n);
 
-    int a = n-100;
-    printf("%d",a);
+    int count = 0;
+    while(n!=0)
+    {
+        if(n>=100)
+        {
+            n-=100;
+            count++;
+        }
+        else if(n>=20)
+        {
+            n-=20;
+            count++;
+        }
+        else if(n>=10)
+        {
+            n-=10;
+            count++;
+        }
+        else if(n>=5)
+        {
+            n-=5;
+            count++;
+        }
+        else if (n<5)
+        {
+            n-=1;
+            count++;
+        }
+    }
+    printf("%d\n",count);
+    
     return 0;
 }
