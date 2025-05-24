6b7779b13b8334551ede2a7bf283629f335fe381
Dh1p <uwazed@gmail.com>
2025-05-24 19:49:48 +0600

    24 May

--- A_Dubstep.c
+++ A_Dubstep.c
@@ -10,18 +10,4 @@ int main()
     char b[201];
 
     scanf("%s",a);
-
-    int l = strlen(a);
-
-    int count=0;
-    for(int i=0; i<l; i++)
-    {
-        if(a[i]='W' && a[i+1]=="U" && a[i+3]=='B')
-        {
-            count++;
-        }
-    }
-    printf("%d",count);
-
-    return 0;
 }
