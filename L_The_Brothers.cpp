ea50dae168c821e7227fac175fdbbd5856bc93f9
Dh1p <uwazed@gmail.com>
2025-06-03 01:17:05 +0600

    3June

---
+++ L_The_Brothers.cpp
@@ -0,0 +1,17 @@
+/**
+ *     DHIP
+**/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string a,b;
+    getline(cin,a);
+    cin.ignore();
+    getline(cin,b);
+
+    cout << a << " " << b;
+    return 0;
+}
